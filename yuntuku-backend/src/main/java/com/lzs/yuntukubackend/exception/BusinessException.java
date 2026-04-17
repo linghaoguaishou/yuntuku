824fdecd96d9e0a4a12e8e834a2879b1d9a7689b
lizhanshuo <204309787@qq.com>
2026-04-17 21:59:14 +0800

    本次提交所做的内容包括： 1:自定义异常类

---
+++ yuntuku-backend/src/main/java/com/lzs/yuntukubackend/exception/BusinessException.java
@@ -0,0 +1,43 @@
+package com.lzs.yuntukubackend.exception;
+
+
+/**
+ * 异常类
+ */
+public class BusinessException extends RuntimeException
+{
+    /**
+     * 表示错误码
+     */
+    private int code;
+
+    /**
+     * 最基础的异常构造方法
+     * @param message 表示异常的信息
+     * @param code 表示错误码
+     */
+    public BusinessException(String message, int code) {
+        super(message);
+        this.code = code;
+    }
+
+    /**
+     * 通过错误码来构造异常类
+     * @param errorCode 错误码
+     */
+    public BusinessException(ErrorCode errorCode)
+    {
+        this(errorCode.getMessage(),errorCode.getCode());
+    }
+
+    /**
+     * 通过错误码和自定义信息来构造异常类
+     * @param errorCode 错误码
+     * @param message 具体异常信息
+     */
+    public BusinessException(ErrorCode errorCode,String message)
+    {
+        this(message,errorCode.getCode());
+    }
+
+}
