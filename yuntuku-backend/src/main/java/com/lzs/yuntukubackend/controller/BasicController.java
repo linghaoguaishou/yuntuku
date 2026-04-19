2ce7697bd1e6d29853e21bba60b31f5909508ee0
lizhanshuo <204309787@qq.com>
2026-04-19 12:57:59 +0800

    Revert "本次提交所做的内容包括："
    This reverts commit 48fa6b7a05dd9dc90fe135a6e778165f7b918791.

--- yuntuku-backend/src/main/java/com/lzs/yuntukubackend/controller/BasicController.java
+++ yuntuku-backend/src/main/java/com/lzs/yuntukubackend/controller/BasicController.java
@@ -16,8 +16,6 @@
 
 package com.lzs.yuntukubackend.controller;
 
-import com.lzs.yuntukubackend.common.BaseResponse;
-import com.lzs.yuntukubackend.common.ResponseUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
